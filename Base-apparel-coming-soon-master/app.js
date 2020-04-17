41a7f4457d4ff5a0db23ac1ee5901e504b3f3b0c
Kamil Komarowski <k.w.komarowski@gmail.com>
2020-04-17 17:45:46 +0200

    Added email validation

--- Base-apparel-coming-soon-master/app.js
+++ Base-apparel-coming-soon-master/app.js
@@ -1,3 +1,22 @@
-window.addEventListener('resize', () => {
-    document.querySelector('.main-section__error-label').textContent = window.innerWidth;
+const form = document.querySelector('.main-section__form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const email = form.querySelector('.main-section__input').value;
+    const errorLabel = form.querySelector('.main-section__error-label');
+    const errorIcon = form.querySelector('.main-section__error-icon');
+    if(validateEmail(email)){
+        errorLabel.textContent = "";
+        errorIcon.classList.remove('main-section__error-icon--active');
+    }
+    else {
+        errorLabel.textContent = "Please provide a valid email";
+        errorIcon.classList.add('main-section__error-icon--active');
+    }
 })
+
+function validateEmail(text) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    return regex.test(text);
+}
